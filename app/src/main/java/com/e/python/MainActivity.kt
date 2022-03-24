9541ae17a1407d47f3caae8d1ed1e0be9feb30fe
Mani <mani89.rajakannu@gmail.com>
2022-03-24 19:28:57 +0530

    Initial commit

---
+++ app/src/main/java/com/e/python/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.e.python
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.chaquo.python.PyObject
+import com.chaquo.python.Python
+import com.chaquo.python.android.AndroidPlatform
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (!Python.isStarted()) {
+            Python.start(AndroidPlatform(this))
+        }
+        val pyInstance = Python.getInstance()
+        val pyModule = pyInstance.getModule("ProcessImage")
+        val pyAttribute = pyModule.callAttr("test")
+        findViewById<TextView>(R.id.tv).text=pyAttribute.toString()
+    }
+}
---
+++ app/src/main/java/com/e/python/python/ProcessImage.py
@@ -0,0 +1,4 @@
+import cv2
+
+def test():
+    return "testing testing"
